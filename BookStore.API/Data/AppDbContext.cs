3e422a2935d02fe4c7b974a8777eca9d45af5512
Inom Dev <77639035+Mirzonom@users.noreply.github.com>
2024-06-02 16:00:15 +0500

    add DbContext

---
+++ BookStore.API/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.API.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Book> Books { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>()
+            .Property(b => b.Id)
+            .ValueGeneratedOnAdd();
+    }
+}
