a503479908afeb8d0303b1db9a21fa249bf98c48
Inom Dev <77639035+Mirzonom@users.noreply.github.com>
2024-06-02 15:54:33 +0500

    add model

---
+++ BookStore.API/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStore.API.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; }
+    public string Category { get; set; }
+    public List<string> Authors { get; set; }
+}
