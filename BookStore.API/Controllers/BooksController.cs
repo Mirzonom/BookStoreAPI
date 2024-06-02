1b9368b84dc9822d5e309aa7467b412c9487f7fd
Inom Dev <77639035+Mirzonom@users.noreply.github.com>
2024-06-02 16:04:00 +0500

    add controller

---
+++ BookStore.API/Controllers/BooksController.cs
@@ -0,0 +1,87 @@
+﻿using BookStore.API.Data;
+using BookStore.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BooksController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public BooksController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetBooks()
+    {
+        return Ok(await _context.Books.ToListAsync());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetBook(int id)
+    {
+        var book = await _context.Books.FindAsync(id);
+
+        if (book == null) return NotFound();
+
+        return Ok(book);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Book>> CreateBook(Book book)
+    {
+        _context.Books.Add(book);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetBook", new { id = book.Id }, book);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutBook(int id, Book book)
+    {
+        if (id != book.Id) return BadRequest();
+
+        _context.Entry(book).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!BookExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteBook(int id)
+    {
+        var book = await _context.Books.FindAsync(id);
+
+        if (book == null) return NotFound();
+
+        _context.Books.Remove(book);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool BookExists(int id)
+    {
+        return _context.Books.Any(e => e.Id == id);
+    }
+}
