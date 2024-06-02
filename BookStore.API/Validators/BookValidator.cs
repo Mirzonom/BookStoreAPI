b03edc10992ceb6ad73579030bc40d8de6d1112b
Inom Dev <77639035+Mirzonom@users.noreply.github.com>
2024-06-02 15:58:32 +0500

    add validator for book

---
+++ BookStore.API/Validators/BookValidator.cs
@@ -0,0 +1,17 @@
+﻿using BookStore.API.Models;
+using FluentValidation;
+
+namespace BookStore.API.Validators;
+
+public class BookValidator : AbstractValidator<Book>
+{
+    public BookValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Укажите id книги");
+        RuleFor(x => x.Title).NotEmpty().WithMessage("Укажите название книги");
+        RuleFor(x => x.Price).NotEmpty().WithMessage("Укажите цену книги");
+        RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("Укажите изоброжение книги");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Укажите категорию книги");
+        RuleFor(x => x.Authors).NotEmpty().WithMessage("Укажите автора-ов книги");
+    }
+}
